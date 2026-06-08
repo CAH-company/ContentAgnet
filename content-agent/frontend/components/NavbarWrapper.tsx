ecd6fa86914c04575595c51b72cb0f9637bcb96c
jabuti <jabutipl.op@gmail.com>
2026-06-08 16:45:23 +0200

    Hide navbar on login page

---
+++ content-agent/frontend/components/NavbarWrapper.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+import { usePathname } from 'next/navigation'
+import { Navbar } from './Navbar'
+
+export function NavbarWrapper() {
+  const pathname = usePathname()
+  if (pathname === '/login') return null
+  return <Navbar />
+}
